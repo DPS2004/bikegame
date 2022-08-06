fe8ea38e7d892cd60d47946a12ea0e540f94aef9
DPS2004 <10176105+DPS2004@users.noreply.github.com>
2022-08-05 22:19:09 -0400

    initial

---
+++ main.lua
@@ -0,0 +1,19 @@
+txtr(0, "overlay/overlay.bmp")
+txtr(2, "tiles/tilesheet.bmp")
+txtr(4, "sprites/spritesheet.bmp")
+
+print("Bike game", 1, 1)
+
+
+while true do
+
+   -- update logic here:
+
+   clear()
+
+   -- graphics updates here:
+	
+	spr(0,50,50)
+	
+   display()
+end
